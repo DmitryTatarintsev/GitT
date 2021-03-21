2387ff34139eaf6b761f1a391af025a8d71cbdf6
Dmitry <dtatarintsev@gmail.com>
2021-03-21 10:51:33 +0300

    Initial Commit

---
+++ GitT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitT
+//
+//  Created by Dmitry Tatarintsev on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
